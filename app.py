8a5ae6ef68e86024056d53413b83c8756329b7fb
BigMun <58153865+BigMun@users.noreply.github.com>
2020-08-30 17:53:47 +0100

    Added comments

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import requests
 
+#HTTP methods
 method_list = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'TRACE']
 
+#Carrying out the method tests
 for method in method_list:
     req = requests.request(method, 'Enter url here')
     print(method, req.status_code, req.reason)
-    #XST = cross site tracing
 
+#Outputting if XST is possible
 if method == 'TRACE' and 'TRACE / HTTP/1.1' in req.text:
     print('XST possible')
+    #XST = Cross Site Tracing
